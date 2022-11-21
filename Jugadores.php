7c5cc9a24833a7f95e7dd62c1c5a7c7e076a5178
antonio <antoniosevillano05@gmail.com>
2022-11-21 00:35:47 -0500

    Cambios menores para extraccion de datos

--- Jugadores.php
+++ Jugadores.php
@@ -44,26 +44,22 @@ $conn = new mysqli($servername, $username, $password, $dbname);
         $queryJugadores = "SELECT * FROM jugadores WHERE Equipo='1' order by Nombre";
         $Jugadores = $conn -> query($queryJugadores);
         $Jugadores -> fetch_all(MYSQLI_ASSOC);      
-        ?>
-        <section class="flex">
-            <?php foreach ($Jugadores as $jugador) { ?><!-- Mostrar jugadores -->
-                <section class="">
-                    <div>
-                        <?php echo '<img src="' . $Jugadore['UrlBandera'] . '" alt="">' ?>
-                    </div>
-                    <div>
-                        <a href="Jugadores.php" target="_blank">
-                            <section class="equipo flex">
-                            
-                            <?php echo '<a href="Jugadores.php" target="_blank" class="titulo">
-                                        <p class="titulo">' . $Equipo['Nombre'] . '</p>
-                                        </a>' ?>
-                            </section>
-                        </a>
-                    </div>
-                </section>
-            <?php } ?>
-        </section>
+        foreach ($Jugadores as $jugador) { ?><!-- Mostrar jugadores -->
+            <section class="marco">
+                <?php echo '<img src="' . $jugador['foto'] . '" alt="">' ?>
+                <?php echo '<h3 class="titulo">' . $jugador['Nombre'] . ' ' . $jugador['Apellido'] .'</h3>' ?>
+                <div>
+                    <?php echo '<p class="titulo">Equipo: ' . $jugador['Equipo'] .'</p>' ?>
+                    <?php echo '<p class="titulo">Número: ' . $jugador['Numero'] .'</p>' ?>
+                    <?php echo '<p class="titulo">Posición: ' . $jugador['Posicion'] .'</p>' ?>
+                    <?php echo '<p class="titulo">Goles Anotados: ' . $jugador['GolesAnotados'] .'</p>' ?>
+                    <?php echo '<p class="titulo">Tarjetas Rojas: ' . $jugador['TarRoja'] .'</p>' ?>
+                    <?php echo '<p class="titulo">Tarjetas Amarillas: ' . $jugador['TarAmarilla'] .'</p>' ?>
+                    <?php echo '<p class="titulo">Nacionalidad: ' . $jugador['Nacionalidad'] .'</p>' ?>
+                    <?php echo '<p class="titulo">Edad: ' . 2022-$jugador['FechaNacimiento'] .'</p>' ?>
+                </div>
+            </section>
+        <?php } ?>
     </div>
 </body>
 </html>
