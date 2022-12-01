ba5c0c5619e6767cac648b2fb151030230784a6e
antonio <antoniosevillano05@gmail.com>
2022-12-01 13:22:43 -0500

    agregado manejo de session en CambioFav

--- CambioFav.php
+++ CambioFav.php
@@ -8,13 +8,18 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 $idEquipo = $_GET['idEquipo'];
 
+if ($_SESSION) {
+    $sql = "UPDATE usuarios SET EFavorito = '$idEquipo' WHERE usuarios.Id = $_SESSION[Id];";
+    $sqlfav = $conn->query($sql);
+    $_SESSION["favorito"] = $idEquipo;
+    header('Location: index.php');
+}else {
+    echo '<section class="error">
+                <img src="https://cdn3.iconfinder.com/data/icons/user-interface-21/32/1026-512.png" alt="">
+                <p class="error"> Para seleccionar Favoritos debes iniciar sesión</p>
+                <a href="index.php">Volver al Inicio</a>
+            </section>';
+}
 
 
-$sql = "UPDATE usuarios SET EFavorito = '$idEquipo' WHERE usuarios.Id = $_SESSION[Id];";
-$sqlfav = $conn->query($sql);
-header('Location: index.php');
-
-
-
-$_SESSION["favorito"] = $idEquipo;
 ?>
